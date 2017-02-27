fede295d59161e1acc216de832bc2bfd48f2ea76
Dylan Kern <dyl.kern@gmail.com>
2017-02-27 14:43:30 -0700

    Adding decision classes and xmls, made icon

---
+++ app/src/main/java/csci448/appigators/snappydecisions/FoodDecisionActivity.java
@@ -0,0 +1,15 @@
+package csci448.appigators.snappydecisions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodDecisionActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_decision);
+    }
+}
---
+++ app/src/main/java/csci448/appigators/snappydecisions/ProductDecisionActivity.java
@@ -0,0 +1,15 @@
+package csci448.appigators.snappydecisions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductDecisionActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_decision);
+    }
+}
