da9842c53b0a413caa1bae9b46a37453bb070655
Dylan Kern <dyl.kern@gmail.com>
2017-03-01 14:22:36 -0700

    Added Crashlytics. Get with it losers.

--- app/src/main/java/csci448/appigators/snappydecisions/StartActivity.java
+++ app/src/main/java/csci448/appigators/snappydecisions/StartActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class StartActivity extends AppCompatActivity
 {
@@ -21,6 +23,7 @@ public class StartActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_start);
 
         randomDecisionButton = (Button)findViewById(R.id.normal_decision_button);
