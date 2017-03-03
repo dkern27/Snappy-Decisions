9bab577dbc31dc66e9dda7f5080e4ece2cd98fd6
Stephen Tracy <Streebit@Loraes-MacBook-Pro.local>
2017-03-03 15:27:39 -0700

    Food near me alpha near done. Experimenting with filters view

--- app/src/main/java/csci448/appigators/snappydecisions/FoodDecisionActivity.java
+++ app/src/main/java/csci448/appigators/snappydecisions/FoodDecisionActivity.java
@@ -4,15 +4,85 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FoodDecisionActivity extends AppCompatActivity
 {
+    Button mFiltersButton;
+    Button mSaveButton;
+    Button mLoadButton;
+    Button mMakeDecisionButton;
+    SeekBar mSeekBar;
+    TextView mDistanceText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_decision);
+
+        mFiltersButton = (Button)findViewById(R.id.filters_button);
+        mSaveButton = (Button)findViewById(R.id.save_button);
+        mLoadButton = (Button)findViewById(R.id.load_button);
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        mDistanceText = (TextView) findViewById(R.id.distance_text);
+        mDistanceText.setText(getString(R.string.distance, mSeekBar.getProgress()));
+        mMakeDecisionButton = (Button)findViewById(R.id.make_decision_button);
+
+        mMakeDecisionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(FoodDecisionActivity.this, "Would open up maps, drop a pin", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFiltersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(FoodDecisionActivity.this, "Opening Filters Popup", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(FoodDecisionActivity.this, "Decision saved (not)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mLoadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(FoodDecisionActivity.this, "Decision was NOT loaded", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                mDistanceText.setText(getString(R.string.distance, progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 
     /**
