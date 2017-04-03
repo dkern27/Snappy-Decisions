31522996e45a60615cf302e849a79ecd83b00a3a
harryk333 <hrkrantz@gmail.com>
2017-04-03 14:02:50 -0600

    removed a commented line of code for dylan

--- app/src/main/java/csci448/appigators/snappydecisions/RandomDecisionActivity.java
+++ app/src/main/java/csci448/appigators/snappydecisions/RandomDecisionActivity.java
@@ -258,7 +258,6 @@ public class RandomDecisionActivity extends AppCompatActivity
 
         final EditText decision_et = new EditText(RandomDecisionActivity.this);
         decision_et.setText(option);
-        //decision_et.setPadding(0,8,0,8);
         LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 3);
         ll.addView(decision_et, lp2);
 
