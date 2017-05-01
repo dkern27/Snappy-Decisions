754e8d49b5a6bac64341a1d6abe6c4a599344834
Dylan Kern <dyl.kern@gmail.com>
2017-04-30 19:19:27 -0600

    Added background to decision in random decision, updated help activity

--- app/src/main/java/csci448/appigators/snappydecisions/HelpActivity.java
+++ app/src/main/java/csci448/appigators/snappydecisions/HelpActivity.java
@@ -2,11 +2,20 @@ package csci448.appigators.snappydecisions;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity
 {
+    TextView mRandomDescription;
 
     public static Intent newIntent(Context packageContext)
     {
@@ -19,5 +28,15 @@ public class HelpActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        mRandomDescription = (TextView)findViewById(R.id.help_random_description);
+        Spannable span = (Spannable)mRandomDescription.getText();
+        ForegroundColorSpan greenSpan = new ForegroundColorSpan(ContextCompat.getColor(this, R.color.colorPrimary));
+        ForegroundColorSpan redSpan = new ForegroundColorSpan(Color.RED);
+        span.setSpan(new StyleSpan(Typeface.BOLD), 58, 62, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        span.setSpan(greenSpan, 58, 62, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        span.setSpan(new StyleSpan(Typeface.BOLD), 112, 117, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        span.setSpan(redSpan, 112, 117, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+
     }
 }
