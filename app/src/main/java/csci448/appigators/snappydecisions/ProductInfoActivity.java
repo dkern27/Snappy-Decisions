d44ffa3b8a84c0e3a93607428a01e8925a959379
Dannister <dherman@mines.edu>
2017-05-05 13:55:39 -0600

    Updated WebView so it works properly as far as I can tell

--- app/src/main/java/csci448/appigators/snappydecisions/ProductInfoActivity.java
+++ app/src/main/java/csci448/appigators/snappydecisions/ProductInfoActivity.java
@@ -6,7 +6,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -30,6 +32,14 @@ public class ProductInfoActivity extends AppCompatActivity {
         mWebView = (WebView)findViewById(R.id.product_web_view);
 
         mUri = getIntent().getData();
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+
         mWebView.loadUrl(mUri.toString());
     }
 
